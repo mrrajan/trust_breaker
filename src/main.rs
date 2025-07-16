e94fde0a96dfd361fca0e46cc3730d51d12f180d
mrrajan <86094767+mrrajan@users.noreply.github.com.>
2025-07-16 17:40:17 +0530

    cleaning up
    Signed-off-by: mrrajan <86094767+mrrajan@users.noreply.github.com.>

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ mod exhort;
 mod osv;
 mod sbom_cdx;
 mod sbom_spdx;
-use crate::exhort::ExhortResponse;
-use crate::osv::{OSVResults, Vulnerability};
 use clap::{Arg, Command};
 use log::{error, info};
 use simplelog::*;
-use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
@@ -32,7 +29,6 @@ async fn main() {
 
     let sbom_file = matches.get_one::<String>("sbom_file").unwrap();
     let sbom_type = matches.get_one::<String>("sbom_type").unwrap();
-    //let exhort_api = matches.get_one::<String>("exhort_api").map(|s| s.as_str()).unwrap_or("");
     CombinedLogger::init(vec![
         TermLogger::new(
             LevelFilter::Info,
@@ -47,17 +43,10 @@ async fn main() {
         ),
     ])
     .unwrap();
-    //let (osvresponse, mut purl_with_vuln) = osv::retrieve_sbom_osv_vulns(&sbom_file).await;
-    //osv::retrieve_sbom_osv_vulns(&sbom_file).await;
-    // if !exhort_api.is_empty(){
-    //     let exhort:ExhortResponse = exhort::get_exhort_response(sbom_file, exhort_api).await;
-    //     compare::compare_exhort_osvnvd(exhort, osvresponse, purl_with_vuln).await;
-    //     info!("Validation completed, Please check the logs for the results");
-    // }
     let mut purl: Vec<String> = Vec::new();
     if sbom_type == "cdx" {
-        let components = sbom_cdx::get_cdx_purl(&sbom_file).await;
-        purl.extend(sbom_cdx::get_cdx_purl_x(components).await);
+        let components = sbom_cdx::get_cdx_components(&sbom_file).await;
+        purl.extend(sbom_cdx::get_cdx_purl(components).await);
     } else if sbom_type == "spdx" {
         let packages = sbom_spdx::get_spdx_sbom_package(&sbom_file).await;
         purl.extend(sbom_spdx::get_spdx_purl(packages).await);
@@ -65,4 +54,8 @@ async fn main() {
         error!("Select sbom_type as either `cdx` or `spdx`");
     }
     osv::retrieve_sbom_osv_vulns(purl, &sbom_type).await;
+    // todo
+    // Retrieve vulnerability information from TPA analyze endpoint
+    // Create comparison logic
+    // Remove exhort.rs and compare.rs files if not needed
 }
